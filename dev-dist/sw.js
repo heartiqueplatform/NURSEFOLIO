214481bf1017648905738791cf5d0debfbaf6b83
HEARTIQUE PLATFORM <heartiqueofficial@gmail.com>
2026-06-05 01:06:18 +0300

    upated meta

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k4rficg7e54"
+    "revision": "0.ikoeic0o4p8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
